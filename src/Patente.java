d140d6eb4a5e2b25cc8c83166896c5761f82e668
ghidi006 <saraghidinelli2020@olivelliputelli.it>
2023-10-13 09:49:02 +0200

    aaaaaaa

--- src/Patente.java
+++ src/Patente.java
@@ -1,27 +1,27 @@
-public class Patente{
-    private String scad_patente;
+// public class Patente{
+//     private String scad_patente;
     
 
-    public Patente (String scad_patente){
-        this.scad_patente = scad_patente;
+//     public Patente (String scad_patente){
+//         this.scad_patente = scad_patente;
         
-    }
-    public Patente (Patente that){
-        this.scad_patente = that.scad_patente;
+//     }
+//     public Patente (Patente that){
+//         this.scad_patente = that.scad_patente;
         
-    }
-    public Patente (){
-        this.scad_patente = "2001-01-01";
+//     }
+//     public Patente (){
+//         this.scad_patente = "2001-01-01";
         
-    }
+//     }
 
-    public String getScad_patente(){
-        return this.scad_patente;
-    }
+//     public String getScad_patente(){
+//         return this.scad_patente;
+//     }
     
-    public void setScad_patente(){
-        this.scad_patente = scad_patente;
-    }
+//     public void setScad_patente(){
+//         this.scad_patente = scad_patente;
+//     }
 
 
-}
+// }
