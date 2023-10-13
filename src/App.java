9ec2f0829d065576f32dc119c799a341ee5d2c74
ghidi006 <saraghidinelli2020@olivelliputelli.it>
2023-10-13 08:59:53 +0200

    ciao

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.time.LocalDate;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         
+        LocalDate oggi = LocalDate.now();
+        System.out.println("oggi: " + oggi);
+
+
     }
 }
--- src/Macchina.java
+++ src/Macchina.java
@@ -1,3 +1,39 @@
-public class Macchina {
-    
+public class Macchina extends Veicolo{
+    private int porte;
+    private int cavalli;
+
+
+    public Macchina (int porte, int cavalli, int cilindrata){
+        this.porte = porte;
+        this.cavalli = cavalli;
+    }
+
+    public Macchina (Macchina that){
+        this.porte = that.porte;
+        this.cavalli = that.cavalli;
+    }
+
+    public Macchina (){
+        this.porte = 70;
+        this.cavalli = 69;
+    }
+
+
+    public int getPorte(){
+        return this.porte;
+    }
+
+    public int getCavalli(){
+        return this.cavalli;
+    }
+
+
+    public void setPorte(){
+        this.porte = porte;
+    }
+
+    public void setCavalli(){
+        this.cavalli = cavalli;
+    }
+
 }
